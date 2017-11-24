e55d75569227153c715e65b9f53df1c5afe32ba4
Zeroalpha <christian.moniatis@gmail.com>
2017-11-24 14:00:22 +0100

    Added energy collection to repair logic

--- repair.js
+++ repair.js
@@ -11,21 +11,41 @@ var helper = require("creep_helper");
 
 var role = {
     run: function(creep){
-        //find structure to repair
-        var targets = creep.room.find(FIND_STRUCTURES,{
-            filter: (struct) => struct.hits < struct.hitsMax
-        });
-        if(targets.length > 0){
-            //sort by damage
-            targets = _.sortBy(targets,function(e){return (e.hitsMax - e.hits)});
-            var target = targets[targets.length - 1];
-            if(creep.repair(target) == ERR_NOT_IN_RANGE){
-                creep.moveTo(target);
+        var src = creep.room.find(FIND_SOURCES)[0];
+        
+        if(creep.memory.full && creep.carry.energy == 0){
+            creep.memory.full = false;
+            creep.say('🔄 harvest');
+        }
+        
+        if(!creep.memory.full && creep.carry.energy == creep.carryCapacity){
+            creep.memory.full = true;
+            creep.say('🔧 repair');
+        }
+        
+        
+        if(creep.memory.full){
+            //find structure to repair
+            var targets = creep.room.find(FIND_STRUCTURES,{
+                filter: (struct) => struct.hits < struct.hitsMax
+            });
+            if(targets.length > 0){
+                //sort by damage
+                targets = _.sortBy(targets,function(e){return (e.hitsMax - e.hits)});
+                var target = targets[targets.length - 1];
+                if(creep.repair(target) == ERR_NOT_IN_RANGE){
+                    creep.moveTo(target);
+                }
             }
+            else{
+                helper.random_move(creep);
+            }            
         }
         else{
-            helper.random_move(creep);
+            helper.m_harvest(creep,src);
         }
+        
+
         
     }
     
